31d1459e13bf65c57e16187f11b4362618b40830
silhouetteamerica <tanderson@silhouetteamerica.com>
2021-11-23 08:35:27 -0700

    SM-6094 #comment adds a space to more easily separate out data #time 5m

--- webhooks/bc-order-updated.php
+++ webhooks/bc-order-updated.php
@@ -2,7 +2,7 @@
 require_once ('../app.php');
 
 // log it for now
-file_put_contents('../logs/order-updated-json.log', file_get_contents("php://input"), FILE_APPEND);
+file_put_contents('../logs/order-updated-json.log', file_get_contents("php://input") . "\n", FILE_APPEND);
 
 $request = file_get_contents("php://input");
 $request_arr = json_decode($request, true);
