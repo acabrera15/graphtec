989b34118b53f635788049c7407e0393a67bcab7
Tony Anderson <tony.anderson@siserna.com>
2023-05-13 08:46:48 -0600

    more logging for the querystring of all GET requests to BigCommerce #time 5m

--- model/Logger.php
+++ model/Logger.php
@@ -1,5 +1,10 @@
 <?php
 trait Logger {
+
+    protected function standard_log_file_name(): string {
+        return get_class($this) . '.log';
+    }
+
     protected function write_to_log(string $log_file_name, string $log_data): false|int {
         return file_put_contents(
             dirname(__FILE__) . '/../logs/' . $log_file_name,
