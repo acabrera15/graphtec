6573122f4b82725fad37f6bd653116ef531417f0
Tony Anderson <tony.anderson@siserna.com>
2023-05-23 15:50:12 -0600

    moves logs to the tmp directory so they get flushed every so often #time 15m

--- model/Logger.php
+++ model/Logger.php
@@ -7,7 +7,7 @@ trait Logger {
 
     protected function write_to_log(string $log_file_name, string $log_data): false|int {
         return file_put_contents(
-            dirname(__FILE__) . '/../logs/' . $log_file_name,
+            sys_get_temp_dir() . '/' . $log_file_name,
             date('Y-m-d H:i:s') . " - {$log_data}\n",
             FILE_APPEND
         );
